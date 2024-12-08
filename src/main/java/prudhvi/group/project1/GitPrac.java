1a8df094f4fb1afffcfb5434006bc8a9579eaa37
prudhvikrishnasai <prudhvikrish08@gmail.com>
2024-12-08 17:59:56 -0500

    Added again

--- src/main/java/prudhvi/group/project1/GitPrac.java
+++ src/main/java/prudhvi/group/project1/GitPrac.java
@@ -7,7 +7,10 @@ public class GitPrac {
     static void methods1(){
 
     }
+<<<<<<< HEAD
     static void methods2(){
         line one1
     }
+=======
+>>>>>>> parent of 9068b54 (resolved merge conflict)
 }
